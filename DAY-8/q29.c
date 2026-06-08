d0522907aee53b84d0f305ef9356b49df4983510
kashnova <kashish.durgeshgupta11@gmail.com>
2026-06-08 11:22:12 +0530

    DAY-8 uploaded

---
+++ DAY-8/q29.c
@@ -0,0 +1,17 @@
+// Write a program to print half pyramid pattern.
+#include <stdio.h>
+int main()
+{
+    int rows, i, j;
+    printf(" Enter the rows :");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ DAY-8/q31.c
@@ -0,0 +1,15 @@
+// Write a program to print character triangle.
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%c", 'A' + j - 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
